628446656319dbd497aa60814d0352b5361abcf3
Nicola Mignoni <58893938+nicomignoni@users.noreply.github.com>
2021-02-11 17:13:55 +0100

    Update test_predictor.py

--- test/test_predictor.py
+++ test/test_predictor.py
@@ -7,10 +7,10 @@ from sklearn.datasets import make_regression, make_classification
 from sklearn.model_selection import train_test_split
 
 class TestPredictor(unittest.TestCase):
- 
+	
 	def test_regressor(self):
 		reg_train, reg_target = make_regression(1000, 20)
-		reg_x_train, reg_x_test, reg_y_train, reg_y_test = train_test_split(train, target, test_size=0.3)
+		reg_x_train, reg_x_test, reg_y_train, reg_y_test = train_test_split(reg_train, reg_target, test_size=0.3)
 		reg_autopred = AutoPredictor(estimator_type="regressor")
 		reg_autopred.fit(reg_x_train, reg_y_train, reg_x_test, reg_y_test)
 
@@ -19,7 +19,7 @@ class TestPredictor(unittest.TestCase):
 
 	def test_classifier(self):
 		clf_train, clf_target = make_classification(1000, 20)
-		clf_x_train, clf_x_test, clf_y_train, clf_y_test = train_test_split(train, target, test_size=0.3)
+		clf_x_train, clf_x_test, clf_y_train, clf_y_test = train_test_split(clf_train, clf_target, test_size=0.3)
 
 		clf_autopred = AutoPredictor(estimator_type="classifier")
 		clf_autopred.fit(clf_x_train, clf_y_train, clf_x_test, clf_y_test)
@@ -27,7 +27,6 @@ class TestPredictor(unittest.TestCase):
 		print(clf_autopred.results)
 		self.assertIsNotNone(clf_autopred.results)
 
-   
 if __name__ == "__main__":
 	unittest.main()
 
